1b13584a79db3c0303fac5fd63e39ced42508614
kikito <kikito@gmail.com>
2012-11-16 17:16:37 +0100

    geom is now able to traverse grids

--- bump/geom.lua
+++ bump/geom.lua
@@ -7,8 +7,23 @@ local geom = {}
 local path = (...):gsub("%.geom$","")
 local util       = require(path .. '.util')
 
-local util_abs = util.abs
+-- private stuff
+
+local abs         = util.abs
 local floor, ceil = math.floor, math.ceil
+
+local function gridTraverseInit(cellSize, t, t1, t2)
+  local v = t2 - t1
+  if     v > 0 then
+    return  1,  cellSize / v, ((t + v) * cellSize - t1) / v
+  elseif v < 0 then
+    return -1, -cellSize / v, ((t + v - 1) * cellSize - t1) / v
+  else
+    return 0, math.huge, math.huge
+  end
+end
+
+-- public stuff
 
 function geom.boxesIntersect(l1,t1,w1,h1, l2,t2,w2,h2)
   return l1 < l2+w2 and l1+w1 > l2 and t1 < t2+h2 and t1+h1 > t2
@@ -19,16 +34,47 @@ function geom.boxesDisplacement(l1,t1,w1,h1, l2,t2,w2,h2)
   local c1y, c2y = (t1+h1) * .5, (t2+h2) * .5
   local dx = l2 - l1 + (c1x < c2x and -w1 or w2)
   local dy = t2 - t1 + (c1y < c2y and -h1 or h2)
-  if util_abs(dx) < util_abs(dy) then return dx,0,dx,dy end
+  if abs(dx) < abs(dy) then return dx,0,dx,dy end
   return 0,dy,dx,dy
 end
 
+function geom.gridCoords(cellSize, x,y)
+  return floor(x / cellSize) + 1, floor(y / cellSize) + 1
+end
+local geom_gridCoords = geom.gridCoords
+
 function geom.gridBox(cellSize, l,t,w,h)
   if not l then return nil end
-  local invSize = 1 / cellSize
-  local gl,gt = floor(l * invSize) + 1, floor(t * invSize) + 1
-  local gr,gb = ceil((l+w) * invSize), ceil((t+h) * invSize)
+  local gl,gt = geom_gridCoords(cellSize, l,t)
+  local gr,gb = ceil((l+w) / cellSize), ceil((t+h) / cellSize)
   return gl, gt, gr-gl, gb-gt
+end
+
+-- based on http://www.cse.yorku.ca/~amana/research/grid.pdf
+function geom.gridTraverse(cellSize, x1,y1, x2,y2, callback)
+  local x, y           = geom_gridCoords(cellSize, x1,y1)
+  local ox, oy         = geom_gridCoords(cellSize, x2,y2)
+
+  local maxLen         = abs(ox-x) + abs(oy-y)
+  local len            = 1
+
+  local stepX, dx, tx  = gridTraverseInit(cellSize, x, x1, x2)
+  local stepY, dy, ty  = gridTraverseInit(cellSize, y, y1, y2)
+
+  if callback(x,y) == false then return len end
+
+  while len <= maxLen and (x~=ox or y~=oy) do
+    if tx < ty then
+      tx = tx + dx
+      x = x + stepX
+    else
+      ty = ty + dy
+      y = y + stepY
+    end
+    len = len + 1
+    if callback(x,y) == false then return len end
+  end
+  return len
 end
 
 return geom
--- spec/geom_spec.lua
+++ spec/geom_spec.lua
@@ -22,11 +22,58 @@ describe('bump.geom', function()
     it("returns nil if l is nil", function()
       assert.equal(nil, geom.gridBox())
     end)
-    it("returns the l,t,w,h of the smallest grid box containing the given box in wc", function()
+    it("returns the l,t,w,h of the smallest grid box(coordinates of top-left grid, plus with and height, or 0,0) containing the given row", function()
       assert.same({1,1,0,0}, {geom.gridBox(64, 1,1,10,10)})
       assert.same({1,1,0,0}, {geom.gridBox(64, 1,1,10,63)})
       assert.same({1,1,0,1}, {geom.gridBox(64, 1,1,10,64)})
       assert.same({1,1,0,0}, {geom.gridBox(64, 1,0,10,10)})
     end)
+  end)
+
+  describe(".gridTraverse", function()
+    local line
+    function draw(x,y)
+      line[#line + 1] = x
+      line[#line + 1] = y
+    end
+    before_each(function()
+      line = {}
+    end)
+
+    it('works', function()
+      geom.gridTraverse(128, 515,515,76,263, draw)
+      assert.same(line, {5,5, 4,5, 4,4, 3,4, 3,3, 2,3, 1,3})
+    end)
+    it('returns the number of cells crossed', function()
+      assert.equal(geom.gridTraverse(128, 600, 128, 500, 512, draw), 5)
+      assert.same(line, {5,2, 5,3, 5,4, 4,4, 4,5})
+    end)
+    it('jumps ok from cell to cell', function()
+      geom.gridTraverse(128, 600, 127, 500, 512, draw)
+      assert.same(line, {5,1, 5,2, 5,3, 5,4, 4,4, 4,5})
+    end)
+
+    describe('When the callback returns false', function()
+      it('stops evaluating', function()
+        local drawUntil44 = function(x,y)
+          line[#line + 1] = x
+          line[#line + 1] = y
+          if x == 4 and y == 4 then return false end
+        end
+        assert.equal(geom.gridTraverse(128, 515,515,76,263, drawUntil44), 3)
+        assert.same(line, {5,5, 4,5, 4,4})
+      end)
+      it('stops evaluating on the first cell too', function()
+        local drawFirstOnly = function(x,y)
+          line[#line + 1] = x
+          line[#line + 1] = y
+          return false
+        end
+        assert.equal(geom.gridTraverse(128, 515,515,76,263, drawFirstOnly), 1)
+        assert.same(line, {5,5})
+      end)
+    end)
+
+
   end)
 end)
