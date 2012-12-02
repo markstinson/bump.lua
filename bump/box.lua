4bf8bb196af92fdada8da73ab3954ded3ef84052
kikito <kikito@gmail.com>
2012-12-02 23:00:01 +0100

    started working on the minkowsky difference

--- bump/box.lua
+++ bump/box.lua
@@ -9,6 +9,10 @@ function box.isIntersecting(l1,t1,w1,h1, l2,t2,w2,h2)
   return l1 < l2+w2 and l1+w1 > l2 and t1 < t2+h2 and t1+h1 > t2
 end
 
+function box.containsPoint(l,t,w,h, x,y)
+  return not(x < l or y < t or x > l + w or y > t + h)
+end
+
 function box.getDisplacement(l1,t1,w1,h1, l2,t2,w2,h2)
   local c1x, c2x = (l1+w1) * .5, (l2+w2) * .5
   local c1y, c2y = (t1+h1) * .5, (t2+h2) * .5
@@ -16,6 +20,13 @@ function box.getDisplacement(l1,t1,w1,h1, l2,t2,w2,h2)
   local dy = t2 - t1 + (c1y < c2y and -h1 or h2)
   if abs(dx) < abs(dy) then return dx,0,dx,dy end
   return 0,dy,dx,dy
+end
+
+function box.getMinkowskyDiff(l1,t1,w1,h1, l2,t2,w2,h2)
+  return l1 - l2 - w2,
+         t2 - t1 - h1,
+         w1 + w2,
+         h1 + h2
 end
 
 function box.getSegmentIntersection(l,t,w,h, x1,y1,x2,y2)
