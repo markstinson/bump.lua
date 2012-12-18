eaef068da9a5216b1b0aa82050ad7e43b1273518
kikito <kikito@gmail.com>
2012-12-18 01:23:53 +0100

    generalized liang-barsky algorithm

--- bump/aabb.lua
+++ bump/aabb.lua
@@ -5,6 +5,40 @@ local util       = require(path .. '.util')
 
 local abs, nearest = util.abs, util.nearest
 
+local function liangBarsky(l,t,w,h, x1,y1,x2,y2, t0,t1)
+  local dx, dy  = x2-x1, y2-y1
+  local p, q, r
+
+  for side = 1,4 do
+    if     side == 1 then p,q = -dx, x1 - l
+    elseif side == 2 then p,q =  dx, l + w - x1
+    elseif side == 3 then p,q = -dy, y1 - t
+    else                  p,q =  dy, t + h - y1
+    end
+
+    if p == 0 then
+      if q < 0 then return nil end
+    else
+      r = q / p
+      if p < 0 then
+        if     r > t1 then return nil
+        elseif r > t0 then t0 = r
+        end
+      else -- p > 0
+        if     r < t0 then return nil
+        elseif r < t1 then t1 = r
+        end
+      end
+    end
+  end
+
+  return t0,t1, dx,dy
+end
+
+function liangBarskyIntersections(l,t,w,h, x1,y1,x2,y1, minT, maxT)
+  local t0,t1,dx,dy = liangBarsky(l,t,w,h, x1,y1,x2,y1, minT, maxT)
+  if t0 then return x1 + dx*t0, y1 + dy*t0, x1 + dx*t1, y1 + dy*t1 end
+end
 
 function aabb.isIntersecting(l1,t1,w1,h1, l2,t2,w2,h2)
   return l1 < l2+w2 and l1+w1 > l2 and t1 < t2+h2 and t1+h1 > t2
@@ -39,35 +73,11 @@ function aabb.getDisplacement(l1,t1,w1,h1, l2,t2,w2,h2)
 end
 
 function aabb.getSegmentIntersection(l,t,w,h, x1,y1,x2,y2)
-  local dx, dy  = x2-x1, y2-y1
+  return liangBarskyIntersections(l,t,w,h, x1,y1,x2,y1, 0,1)
+end
 
-  local t0, t1  = 0, 1
-  local p, q, r
-
-  for side = 1,4 do
-    if     side == 1 then p,q = -dx, x1 - l
-    elseif side == 2 then p,q =  dx, l + w - x1
-    elseif side == 3 then p,q = -dy, y1 - t
-    else                  p,q =  dy, t + h - y1
-    end
-
-    if p == 0 then
-      if q < 0 then return nil end
-    else
-      r = q / p
-      if p < 0 then
-        if     r > t1 then return nil
-        elseif r > t0 then t0 = r
-        end
-      else -- p > 0
-        if     r < t0 then return nil
-        elseif r < t1 then t1 = r
-        end
-      end
-    end
-  end
-
-  return x1 + t0 * dx, y1 + t0 * dy, x1 + t1 * dx, y1 + t1 * dy
+function aabb.getRayIntersection(l,t,w,h, x1,y1,x2,y2)
+  return liangBarskyIntersections(l,t,w,h, x1,y1,x2,y1, 0,math.huge)
 end
 
 return aabb
